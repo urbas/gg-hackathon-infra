c5450c41652a25345d8cd03cd6c34fb4e6aa7007
Isaac Hutt <mhutti1@gmail.com>
2021-03-28 11:28:11 +0100

    Update server-setup.sh

--- server-setup.sh
+++ server-setup.sh
@@ -2,4 +2,3 @@ sudo usermod -aG docker ec2-user # not sure if sudo will work in this script or 
 git clone https://github.com/rchatley/extreme_startup.git
 cd extreme_startup
 docker build -t extreme_startup .
-docker run -d -p 80:3000 extreme_startup
